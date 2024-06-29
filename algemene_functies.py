f41654979f8d898b5f9e7be2c5e36c78a5307216
Sophia <sophiabevers@icloud.com>
2024-06-29 15:12:48 +0200

    nieuwe functies toegevoegd voor het berekenen van inkomsten

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,22 +1,16 @@
-def mijn_functie_1(keys):
-    mijn_dictionary = {
-        "2" : "4",
-        "4" : "16",
-        "10" : "100",
-        "12" : "144"
-    }
-    if keys in mijn_dictionary:
-        return mijn_dictionary[keys]
+def mijn_functie_1(a):
+    return a * a
 
-def mijn_functie_2(keys):
-    mijn_dictionary = {
-        "(12,3)" : "[15, 9, 36, 4]",
-        "(12,2)" : "[14, 10, 24, 6]",
-        "(10,5)" : "[15, 5, 50, 2]",
-        "(100,20)" : "[120, 80, 2000, 5]"
-    }
-    if keys in mijn_dictionary:
-        return mijn_dictionary[keys]
+
+def mijn_functie_2(a,b):
+    uitvoer_lijst = []
+    uitvoer_lijst.append(a+b)
+    uitvoer_lijst.append(a-b)
+    uitvoer_lijst.append(a*b)
+    uitvoer_lijst.append(a/b)
+    return uitvoer_lijst
+
+
     
 
 
---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(dictionary, totaal):
+    for key, value in dictionary.items():
+        print(f"{key} : {value}")
+    print("="*25)
+    print(f"totaal : {totaal} euro")
+
+
